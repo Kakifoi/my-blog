5e94b21dac064b764ec42962132eb772c0acd24b
カキフォイ <r1driving@gmail.com>
2026-04-26 18:03:38 +0900

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'My Blog';
+export const SITE_DESCRIPTION = '最新の情報をお届けするブログサイトです。';
---
+++ start-dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH="/opt/homebrew/bin:$PATH"
+cd /Users/kakiemon/Sites/my-blog
+npm run dev
