21377f32af75cf69a063b68dbae6e16bf068290e
カキフォイ <r1driving@gmail.com>
2026-04-26 18:52:36 +0900

    サイト名を変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'My Blog';
-export const SITE_DESCRIPTION = '最新の情報をお届けするブログサイトです。';
+export const SITE_TITLE = 'カキフォイの日記';
+export const SITE_DESCRIPTION = 'カキフォイの日常をつづった日記ブログです。';
